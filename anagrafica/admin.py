2f3f465b8729fac7c551df80597ef70054ed8ab9
Biodolo <biodolo@gmail.com>
2012-11-11 10:12:22 +0100

    aggiunto sistema di check dei vincoli sulla form delle schede ritiro

--- anagrafica/admin.py
+++ anagrafica/admin.py
@@ -6,9 +6,44 @@ Created on 01/nov/2012
 from models import *
 from django.contrib import admin
 
+class ReadonlyTabularInline(admin.TabularInline):
+    can_delete = False
+    extra = 0
+    editable_fields = []
+    
+    def get_readonly_fields(self, request, obj=None):
+        fields = []
+        for field in self.model._meta.get_all_field_names():
+            if (not field == 'id'):
+                if (field not in self.editable_fields):
+                    fields.append(field)
+        return fields
+    
+    def has_add_permission(self, request):
+        return False
+    
 admin.site.register(Apparecchio)
 admin.site.register(Operatore)
 
+class SchedaRitiroAdmin(admin.ModelAdmin):
+    class Media:
+        js=['/js/scheda_ritiro.js']
+        
+    model = SchedaRitiro
+    search_fields = ('utente__cognome', '=progressivo')
+    list_filter = ('ritirato_da__nome','apparecchio__tipo','apparecchio__marca','apparecchio__modello','apparecchio',)
+    list_display = ('progressivo','apparecchio', 'utente', 'data_ritiro', 'ritirato_da')
+    raw_id_fields = ('apparecchio','utente')
+    
+class SchedaRitiroInline(ReadonlyTabularInline):    
+    model=SchedaRitiro
+    fieldsets = (
+            (None, {
+                'fields' : ('progressivo','apparecchio', 'utente', 'data_ritiro', 'garanzia', 'ritirato_da')
+            }),
+            )
+        
+admin.site.register(SchedaRitiro, SchedaRitiroAdmin)
 class ClienteAdmin(admin.ModelAdmin):
     model = Cliente
     search_fields = ('cognome','nome','citta')
@@ -21,20 +56,9 @@ class ClienteAdmin(admin.ModelAdmin):
                 'fields' : ('citta', 'indirizzo', 'telefono', 'cellulare')
                 })
                )
+    inlines = [SchedaRitiroInline]
 
 
 admin.site.register(Cliente, ClienteAdmin)
 
-class SchedaRitiroAdmin(admin.ModelAdmin):
-    class Media:
-        js=['/js/toggle_filter.js','/js/scheda_ritiro.js']
-        
-    model = SchedaRitiro
-    search_fields = ('utente__cognome', '=progressivo')
-    list_filter = ('ritirato_da__nome','apparecchio__tipo','apparecchio__marca','apparecchio__modello','apparecchio',)
-    list_display = ('progressivo','apparecchio', 'utente', 'data_ritiro', 'ritirato_da')
-    raw_id_fields = ('apparecchio','utente')
-    
-    
-        
-admin.site.register(SchedaRitiro, SchedaRitiroAdmin)
+
--- js/scheda_ritiro.js
+++ js/scheda_ritiro.js
@@ -1,8 +1,44 @@
-(function($, key_field, key_type, form_id, submit_check) {
-
+(function($){
 	var form_val = new Array();
 	var selected_key = false;
+	var form_readonly = false;
+	var key_field=[ 'id_apparecchio', 'id_utente' ];
+	var key_type={
+			'id_apparecchio' : 'Apparecchio',
+			'id_utente' : 'Cliente'
+		};
+	var form_id='schedaritiro_form';
 
+
+
+	function isEmpty(id){
+		return $("#"+id).val()=='';
+	}
+	
+	function isChecked(id){
+		return $("#"+id).is(':checked');
+	}
+	
+	
+	function mustFill(id,message){
+		if (isEmpty(id)){
+			alert(message);
+			$("#"+id).focus();
+			return true;
+		}
+		return false;
+	}
+	
+	function mustCheck(id,message){
+		if (!isChecked(id)){
+			alert(message);
+			$("#"+id).focus();
+			return true;
+		}
+		return false;
+	}
+	
+	
 	function mostraRelated(id) {
 		var value = $('#' + id).val();
 		var classe = key_type[id];
@@ -29,51 +65,73 @@
 		});
 	}
 
-	$(document)
-			.ready(
-					function() {
-						var functionBackup = new Array();
-						functionBackup['dismissRelatedLookupPopup'] = dismissRelatedLookupPopup;
-						dismissRelatedLookupPopup = function(win, val) {
-							functionBackup['dismissRelatedLookupPopup'](win,
-									val);
-							if (selected_key) {
-								mostraRelated(selected_key);
-								selected_key = false;
-							}
-						};
-						for (i = 0; i < key_field.length; i++) {
-							setupRelated(key_field[i]);
-						}
-						if (submit_check)
-							$('#'+form_id).submit(submit_check($));
-					});
-
-})(
-		django.jQuery, 
-		[ 'id_apparecchio', 'id_utente' ], 
-		{
-			'id_apparecchio' : 'Apparecchio',
-			'id_utente' : 'Cliente'
-		},
-		'schedaritiro_form',
-		function($) {
-			return function(){
-				if ($('#id_garanzia').is(':checked')){
-					// se viene indicato che l'apparecchio è in garanzia
-					if ($("#id_data_acquisto").val()==''){
-						alert('Indicare data di acquisto');
-						$("#id_data_acquisto").focus();
-						return false;
-					}
-					if ($("#id_tipo_prova_acquisto").val()==''){
-						alert('Indicare il tipo di prova d\'acquisto');
-						$("#id_tipo_prova_acquisto").focus();
-						return false;
-					}
-					//if ($"#)
+	$(document).ready(
+		function() {
+			var functionBackup = new Array();
+			functionBackup['dismissRelatedLookupPopup'] = dismissRelatedLookupPopup;
+			dismissRelatedLookupPopup = function(win, val) {
+				functionBackup['dismissRelatedLookupPopup'](win,
+						val);
+				if (selected_key) {
+					mostraRelated(selected_key);
+					selected_key = false;
 				}
-				return true;
 			};
+			for (i = 0; i < key_field.length; i++) {
+				setupRelated(key_field[i]);
+			}
+			if (submit_check)
+				$('#'+form_id).submit(submit_check);
+			if ($('#id_riconsegnato_da').val()!=''){
+				form_readonly=true;
+			}
+
+		});
+	
+	function submit_check(){
+		if (form_readonly) {
+			if (
+					(prompt("L'apparecchio e' gia' stato ritirato, inserire la password di conferma")!='qui ci metto la password di conferma')&&
+					(prompt("L'apparecchio e' gia' stato ritirato, inserire la password di conferma")!='qui ci metto la password di conferma')&&
+					(prompt("L'apparecchio e' gia' stato ritirato, inserire la password di conferma")!='qui ci metto la password di conferma')){
+				alert("Impossibile modificare la scheda ritiro, il tentativo e' stato registrato");
+				return false;
+			}
 		}
-	);
+		if (isChecked('id_garanzia')){
+			if (	// APPARECCHIO IN GARANZIA
+					mustCheck("id_acquisizione_garanzia","Non e' possibile ritirare apparecchiature in garanzia senza prova d'acquisto") ||
+					mustFill("id_data_acquisto","Indicare data di acquisto") ||
+					mustFill("id_tipo_prova_acquisto","Indicare il tipo di prova d'acquisto")
+				)
+			{
+				return false;	
+			}
+		}else{
+			// APPARECCHIO FUORI GARANZIA
+			if (
+					mustFill("id_acconto","Non e' possibile ritirare apparecchiature fuori garanzia senza acconto") ||
+					mustFill("id_scontrino_acconto","Indicare il numero dello scontrino dell'acconto")
+				)
+			{
+				return false;	
+			}
+			if (isChecked("id_preventivo_ok") && mustFill("id_importo_preventivo","Non e' possibile ritirare apparecchiature fuori garanzia senza indicare l'importo del preventivo")){
+				return false;
+			}			
+		}
+		if (
+				( 
+					$('#id_riconsegnato_da').val()!='' &&
+					(
+							mustFill("id_data_riconsegna","Indicare data di riconsegna") 
+					)
+				) || ( 
+					false
+				)
+			){
+			return false;
+		}
+		return true;
+	}
+})(django.jQuery);
